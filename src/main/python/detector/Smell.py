fc0668fe67c1c278963470a13493ff6f5ae3f532
Massiwen <massiwen@outlook.com>
2021-03-03 19:26:58 -0500

    Optimize Smell data format

--- src/main/python/detector/Smell.py
+++ src/main/python/detector/Smell.py
@@ -1,6 +1,7 @@
 import os
 
-import pandas as pd
+# import shutil
+# import pandas as pd
 
 from src.main.python.detector.DetectorStrategy import DetectorStrategy
 
@@ -9,36 +10,46 @@ class Smell(DetectorStrategy):
 
     def __init__(self, cfg: dict):
         super(Smell, self).__init__(cfg)
+        self.id = 0
+        self.temp_path = str()
 
     def _execute_jar(self, repo_cfg: dict):
         print('\n\n*** Run Code Smell Detector ***')
         # init param to execute .jar
         jar_path = os.path.abspath(self.cfg['paths']['designite_java'])
         repo_path = os.path.abspath(self.cfg['paths']['repo'] + repo_cfg['name'])
-        out_path = os.path.abspath(self.cfg['paths']['smell_report'] + '/temp')
+        self.temp_path = os.path.abspath(self.cfg['paths']['smell_report'] + '/temp')
 
         # execute .jar
-        os.system("java -jar " + jar_path + " -i " + repo_path + " -o " + out_path)
+        os.system("java -jar " + jar_path + " -i " + repo_path + " -o " + self.temp_path)
 
     def _format_data(self, repo_cfg: dict, commit_hash: str):
         print('\n\n*** Formatting Code Smell Data ***')
-        temp_path = os.path.abspath(self.cfg['paths']['smell_report'] + '/temp')
-        out_path = self.cfg['paths']['smell_report'] + repo_cfg['name']
-        for file in os.listdir(temp_path):
+        print(f"Commit: {commit_hash}")
+
+        self.id += 1
+
+        for file in os.listdir(self.temp_path):
             if file.endswith(".csv"):
-                temp_file_path = temp_path + '/' + file
-                out_file_path = out_path + '/' + file
+                os.rename(self.temp_path + '/' + file, self.temp_path + f'/{str(self.id).zfill(6)}_' + file)
 
-                new_file = pd.read_csv(temp_file_path, index_col=1)
-                new_file['commit_hash'] = commit_hash
-
-                if os.path.exists(out_file_path):
-                    old_file = pd.read_csv(out_file_path, index_col=1)
-                    old_file = old_file.append(new_file, ignore_index=True)
-                    old_file.to_csv(out_file_path, index=False)
-                else:
-                    try:
-                        os.mkdir(out_path)
-                    except:
-                        pass
-                    new_file.to_csv(out_file_path, index=False)
+        #         new_file.set_index('commit_hash', inplace=True)
+        #         if not os.path.exists(out_path):
+        #             os.mkdir(out_path)
+        #         new_file.to_csv(out_file_path, index=False)
+        #
+        #         if os.path.exists(out_file_path):
+        #             old_file = pd.read_csv(out_file_path, header=0)
+        #             old_file.set_index("commit_hash", inplace=True)
+        #
+        #             new_file = old_file.join(new_file,
+        #                                      how='outer',
+        #                                      on='commit_hash',
+        #                                      rsuffix='rsfx_')
+        #             # remove some columns
+        #             # new_file = new_file[new_file.columns.drop(list(new_file.filter(regex='^rsfx_.*$')))]
+        #
+        #         elif not os.path.exists(out_path):
+        #             os.mkdir(out_path)
+        #         new_file.to_csv(out_file_path, index=False)
+        # shutil.rmtree(temp_path)
