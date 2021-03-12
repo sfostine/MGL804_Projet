e6d9522f58a47782cabe730b713277e43880b786
Massiwen <massiwen@outlook.com>
2021-03-12 16:42:49 -0500

    Make cloning repos faster with multi-thread

--- src/main/python/RepoHelper.py
+++ src/main/python/RepoHelper.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import shutil
+import threading
 
 import pandas as pd
 from pydriller import RepositoryMining, GitRepository
@@ -24,6 +25,7 @@ class RepoHelper:
         """
         repo_path = self.cfg['paths']['repo']
         since = datetime.datetime.strptime(self.cfg['filters']['since'], self.cfg['filters']['date_format'])
+        threads = []
         # INITIALIZE ALL REPOS
         for repo in self.cfg["repos"]:
             if (not self.cfg['replace_existing_repo']) and (os.path.exists(repo_path + repo['name'])):
@@ -34,54 +36,62 @@ class RepoHelper:
                     shutil.rmtree(repo_path + repo['name'])
                 # creat directory
                 os.makedirs(repo_path + repo['name'])
+                thread = threading.Thread(target=self.clone_repo, args=(repo_path,since,repo,),name=f"task_{repo['name']}")
+                thread.start()
+                threads.append(thread)
 
-                # cloning repository from remote
-                print('cloning to {}'.format(repo_path + repo['name']))
-                data = pd.DataFrame(columns=self.cfg['commit_columns'])
+        # Wait all finished cloning
+        for t in threads:
+            t.join()
 
-                for commit in RepositoryMining(repo['remote_repo'],
-                                               only_modifications_with_file_types=self.cfg['filters']['file_types'],
-                                               only_in_branch=repo['branch'],
-                                               since=since,
-                                               clone_repo_to=repo_path).traverse_commits():
-                    # build dictionary containing modifications
-                    mdf_detail = {}
-                    mdf_files = []
-                    mdf_methods = []
-                    for modif in commit.modifications:
-                        # TODO remove print
-                        if modif.filename in mdf_detail.keys():
-                            print(f'filename{modif.filename} en double')
-                        mdf_files.append(modif.filename)
+    def clone_repo(self, repo_path, since, repo):
+        # cloning repository from remote
+        print('cloning to {}'.format(repo_path + repo['name']))
+        data = pd.DataFrame(columns=self.cfg['commit_columns'])
 
-                        mdf_detail.update({
-                            modif.filename: {
-                                "old_path": modif.old_path,
-                                "new_path": modif.new_path,
-                                "change_type": modif.change_type,
-                                "cyclomatic": modif.complexity,
-                                "changed_methods": []
-                            }
-                        })
+        for commit in RepositoryMining(repo['remote_repo'],
+                                       only_modifications_with_file_types=self.cfg['filters']['file_types'],
+                                       only_in_branch=repo['branch'],
+                                       since=since,
+                                       clone_repo_to=repo_path).traverse_commits():
+            # build dictionary containing modifications
+            mdf_detail = {}
+            mdf_files = []
+            mdf_methods = []
+            for modif in commit.modifications:
+                # TODO remove print
+                if modif.filename in mdf_detail.keys():
+                    print(f'filename{modif.filename} en double')
+                mdf_files.append(modif.filename)
 
-                        for method in modif.changed_methods:
-                            mdf_detail[modif.filename]['changed_methods'] += [method.name]
-                            mdf_methods.append(method.name)
+                mdf_detail.update({
+                    modif.filename: {
+                        "old_path": modif.old_path,
+                        "new_path": modif.new_path,
+                        "change_type": modif.change_type,
+                        "cyclomatic": modif.complexity,
+                        "changed_methods": []
+                    }
+                })
 
-                    data = data.append(
-                        {
-                            "hash": commit.hash,
-                            "author_date": commit.author_date,
-                            "is_merge": commit.merge,
-                            "nb_files": commit.files,
-                            "files": mdf_files,
-                            "methods": mdf_methods,
-                            "nb_deletions": commit.deletions,
-                            "nb_insertions ": commit.insertions,
-                            "modifications": str(mdf_detail)
-                        }, ignore_index=True
-                    )
-                data.to_csv(self.cfg['paths']['commit_report'] + repo['commit_file'], index=False)
+                for method in modif.changed_methods:
+                    mdf_detail[modif.filename]['changed_methods'] += [method.name]
+                    mdf_methods.append(method.name)
+
+            data = data.append(
+                {
+                    "hash": commit.hash,
+                    "author_date": commit.author_date,
+                    "is_merge": commit.merge,
+                    "nb_files": commit.files,
+                    "files": mdf_files,
+                    "methods": mdf_methods,
+                    "nb_deletions": commit.deletions,
+                    "nb_insertions ": commit.insertions,
+                    "modifications": str(mdf_detail)
+                }, ignore_index=True
+            )
+        data.to_csv(self.cfg['paths']['commit_report'] + repo['commit_file'], index=False)
 
     def add_detectors(self, detector: DetectorStrategy):
         self.detectors.append(detector)
